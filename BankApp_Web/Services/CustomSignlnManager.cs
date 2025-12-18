129477b72a4153644e5fc768a7983ff8d53ec484
huzeyfeyer <huzeyfeyer1@gmail.com>
2025-12-18 17:01:35 +0100

    Voeg CustomSignInManager toe
    Implementeert e-mailgebaseerde aanmelding, registreert de custom SignInManager in DI en werkt Identity/cookie-instellingen bij.

---
+++ BankApp_Web/Services/CustomSignlnManager.cs
@@ -0,0 +1,53 @@
+﻿using BankApp_Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace BankApp_Web.Services
+{
+    // Custom SignInManager die email OF username accepteert bij login
+    public class CustomSignInManager : SignInManager<BankUser>
+    {
+
+        private readonly UserManager<BankUser> _userManager;
+
+        public CustomSignInManager(
+            UserManager<BankUser> userManager,
+            IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<BankUser> claimsFactory,
+            IOptions<IdentityOptions> optionsAccessor,
+            ILogger<SignInManager<BankUser>> logger,
+            IAuthenticationSchemeProvider schemes,
+            IUserConfirmation<BankUser> confirmation)
+            : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+        {
+
+            _userManager = userManager;
+        }
+
+        // Override PasswordSignInAsync om email EN username te ondersteunen
+        public override async Task<SignInResult> PasswordSignInAsync(string userNameOrEmail, string password, bool isPersistent, bool lockoutOnFailure)
+        {
+            // Probeer eerst als username
+            // Zoek gebruiker op username
+            var user = await _userManager.FindByNameAsync(userNameOrEmail);
+
+            // Als niet gevonden, probeer als email
+            if (user == null)
+            {
+
+                user = await _userManager.FindByEmailAsync(userNameOrEmail);
+            }
+
+            if (user == null)
+            {
+
+                return SignInResult.Failed;
+            }
+
+            return await PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
+        }
+    }
+}
+
