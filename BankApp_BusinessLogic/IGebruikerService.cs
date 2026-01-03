7f7e4dac88e93d4358f555904ecd3e34e14a3d97
Tyvian10 <binanaapuassa@outlook.com>
2026-01-03 21:56:36 +0100

    Gebruikerservices en igebruikerservice geconfigureert

---
+++ BankApp_BusinessLogic/IGebruikerService.cs
@@ -0,0 +1,17 @@
+﻿using BankApp_Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BankApp_BusinessLogic
+{
+    public interface IGebruikerService
+    {
+        Task<BankUser?> GetGebruikerByIdAsync(string gebruikerId);
+        Task<BankUser?> GetGebruikerByEmailAsync(string email);
+        Task<List<BankUser>> GetAlleGebruikersAsync();
+        Task<bool> UpdateGebruikerAsync(BankUser gebruiker);
+        Task<bool> VerwijderGebruikerAsync(string gebruikerId);
+        Task<bool> BlokkeerGebruikerAsync(string gebruikerId);
+        Task<bool> DeblokkeerGebruikerAsync(string gebruikerId);
+    }
+}
